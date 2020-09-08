566d92862ebe5f70a5ad67520f62edf5f25fe6f5
jmczerwinska <asia.frej@gmail.com>
2020-09-08 19:31:01 +0200

    display total incomes, expenses and budget balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Balance = () => {
+  const { state } = useContext(GlobalContext);
+  const incomes = state.filter(transaction => transaction.type === "income");
+  const expenses = state.filter(transaction => transaction.type === "expense");
+
+  const totalIncome = incomes
+    .map(transaction => transaction.amount)
+    .reduce((a, b) => a + b, 0);
+
+  const totalExpense = expenses
+    .map(transaction => transaction.amount)
+    .reduce((sum, amount) => sum += amount, 0);
+
   return (
     <div>
       <h2>Your Balance</h2>
-      <h3>$0.00</h3>
+  <h3>${totalIncome - totalExpense}</h3>
       <div>
         <h3>Income</h3>
-        <p> +$0.00</p>
+        <p> +${totalIncome}</p>
       </div>
       <div>
         <h3>Expenses</h3>
-        <p>-$0.00</p>
+        <p>-${totalExpense}</p>
       </div>
     </div>
   )
